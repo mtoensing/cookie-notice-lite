7fbd32fdddccd99cb12f4632441e51648a039eb3
Marc Tönsing <marc@marc.tv>
2020-06-16 16:42:50 +0200

    Initial commit

---
+++ cookie-notice.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+Plugin Name: Cookie Notice Lite (Deutsch)
+Description: Displays a German cookie notice.
+GitHub Plugin URI: mtoensing/cookie-notice-lite
+Version:     0.1
+Author:      MarcDK
+Author URI:  https://marc.tv
+License URI: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+add_action('wp_footer', 'add_cookie_notice');
+
+function add_cookie_notice(){
+	$url = get_privacy_policy_url();
+	$link = '';
+	if (get_privacy_policy_url() != ''){
+		$link = '<a rel="nofollow" href="' . $url . '">Mehr Informationen</a>.';
+	}
+	echo '<p id="cookie-notice">Diese Webseite verwendet Cookies. In dem Du hier fortfährst, stimmst Du der Nutzung der Cookies zu. ' . $link. '<br><button onclick="acceptCookie();">Hab\'s verstanden</button></p>';
+	echo '<script>function acceptCookie(){document.cookie="cookieaccepted=1; expires=Thu, 18 Dec 2030 12:00:00 UTC; path=/",document.getElementById("cookie-notice").style.visibility="hidden"}document.cookie.indexOf("cookieaccepted")<0&&(document.getElementById("cookie-notice").style.visibility="visible");</script>';
+	echo '<style>#cookie-notice{color:#fff;font-family:inherit;background:#0073aa;padding:20px;position:fixed;bottom:10px;left:10px;width:100%;max-width:300px;box-shadow:0 10px 20px rgba(0,0,0,.2);border-radius:5px;margin:0;visibility:hidden;z-index:1000000;box-sizing:border-box}#cookie-notice button{color:inherit;background:#005882;border:0;padding:10px;margin-top:10px;width:100%;cursor:pointer}#cookie-notice a{color:#fff;text-decoration:underline}@media only screen and (max-width:600px){#cookie-notice{max-width:100%;bottom:0;left:0;border-radius:0}}</style>';
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== Lightweight Cookie Notice ===
+Contributors: MarcDK
+Tags: marctv, cookie, notice, consent
+Requires at least: 5.0
+Tested up to: 5.0
+Stable tag: 1.0
+GitHub Plugin URI: mtoensing/lightweight-cookie-notice
+
+== Description ==
+
+Displays a cookie notice. Idea and code from https://wpspeedmatters.com/fastest-cookie-consent-wordpress-plugin/
+
+== Installation ==
+
+* Install plugin
+* Activate it
+
+== Changelog ==
+
+1.0 Initial version
