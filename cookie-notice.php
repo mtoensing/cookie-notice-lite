cc972c921c7dcc573f05df6e59c46fb8cdf1766e
Marc Toensing <marc.toensing@gmail.com>
2021-12-16 12:47:29 +0100

    1.5

--- cookie-notice.php
+++ cookie-notice.php
@@ -3,7 +3,7 @@
 * Plugin Name: Cookie Notice Lite (Deutsch)
 * Description: Displays a German cookie notice.
 * GitHub Plugin URI: mtoensing/cookie-notice-lite
-* Version:     1.0
+* Version:     1.5
 * Author:      MarcDK
 * Author URI:  https://marc.tv
 * License: GPL v2 or later
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: MarcDK
 Tags: marctv, cookie, notice, consent
 Requires at least: 5.0
 Tested up to: 5.0
-Stable tag: 1.0
+Stable tag: 1.5
 GitHub Plugin URI: mtoensing/cookie-notice-lite
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
